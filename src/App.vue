7d29894ee56296424ff658a9029d0246b5e37825
Fu-XDU <fuming.137@icloud.com>
2025-04-14 03:56:03 +0800

    Add anti burn for OLED screen

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import Float from './components/Float.vue'
   <meta name="apple-mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
   <main id="app">
-   <Float/>
+   <Float class="clock"/>
   </main>
 </template>
 
@@ -26,5 +26,34 @@ html, body {
   align-items: center;
   justify-content: center;
   margin: 0;
+}
+
+.clock {
+  animation: antiBurnMove 60s infinite linear,
+  antiBurnOpacity 80s infinite ease-in-out,
+  antiBurnTransform 120s infinite ease-in-out;
+  will-change: transform, opacity;
+}
+
+@keyframes antiBurnMove {
+  0%   { transform: translate(0px, 0px); }
+  25%  { transform: translate(1px, 0px); }
+  50%  { transform: translate(1px, 1px); }
+  75%  { transform: translate(0px, 1px); }
+  100% { transform: translate(0px, 0px); }
+}
+
+@keyframes antiBurnOpacity {
+  0%, 100% { opacity: 0.96; }
+  50%      { opacity: 0.88; }
+}
+
+@keyframes antiBurnTransform {
+  0%, 100% {
+    transform: scale(1) rotate(0deg);
+  }
+  50% {
+    transform: scale(1.01) rotate(0.3deg);
+  }
 }
 </style>
